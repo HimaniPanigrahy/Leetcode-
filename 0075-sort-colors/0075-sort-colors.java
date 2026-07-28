918ed05c76f51256d946179accfc0c440a76db02
Himani Panigrahy <himanipanigrahy28@gmail.com>
2026-07-29 01:21:38 +0530

    Time: 0 ms (100%), Space: 43.2 MB (89.9%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void sortColors(int[] arr) {
+       int n = arr.length ;
+       int low = 0 ;
+       int mid = 0 ;
+       int high = n-1 ;
+       while(mid<=high)
+       {
+        if(arr[mid]==0)
+        {
+            int temp = arr[low] ;
+            arr[low]=arr[mid] ;
+            arr[mid]=temp ;
+            low++ ;
+            mid++ ;
+        }
+        else if(arr[mid]==1)
+        {
+            mid++ ;
+        }
+        else if(arr[mid]==2)
+        {
+            int t = arr[mid] ;
+            arr[mid]=arr[high] ;
+            arr[high]=t ;
+            high-- ;
+        }
+       }
+    }
+}
