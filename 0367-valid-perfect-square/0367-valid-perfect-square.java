945ff93e1d9e44f0c2b6afd602a373fd4caf79fb
Himani Panigrahy <himanipanigrahy28@gmail.com>
2026-08-18 19:42:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (58.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int l =0;
+        int r = num ;
+        while(l<=r)
+        {
+            int mid = l+((r-l)/2) ;
+            long s = (long)mid*mid ;
+            if(s==num) return true ;
+            else if(s>num) r=mid-1 ;
+            else l= mid+1 ;
+        }
+        return false ;
+    }
+}
