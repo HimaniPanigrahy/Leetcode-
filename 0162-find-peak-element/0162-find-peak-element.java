817f5e7bfc8535d7aef1f1932550a9c0a492462e
Himani Panigrahy <himanipanigrahy28@gmail.com>
2026-08-21 01:57:47 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.46%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -11,11 +11,11 @@ class Solution {
             if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]) return mid ;
             else if (nums[mid]<nums[mid-1])r=mid-1 ;
             else if(nums[mid]<nums[mid+1]) l = mid+1 ;
-            else
-            {
-                l++ ;
-                r-- ;
-            }
+            // else
+            // {
+            //     l++ ;
+            //     r-- ;
+            // }
         }
        return 0 ;
     }
