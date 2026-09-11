2926d34ccaaf9da3526b55af100e8aa97f91b1ca
Himani Panigrahy <himanipanigrahy28@gmail.com>
2026-09-11 21:36:17 +0530

    Time: 0 ms (100%), Space: 42.7 MB (11.69%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int temp =n;
+        int count =0 ;
+        while(n!=0)
+        {
+            int d=n%2 ;
+            if(d==1) count++ ;
+            n=n/2 ;
+        }
+        return count ;
+    }
+}
